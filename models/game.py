f2ad081f45695ff653fa62cd898352092dfa3bf3
Ewan Brain <ewan.brain@gmail.com>
2020-12-04 22:34:00 +0000

    rock paper scissors logic done

--- models/game.py
+++ models/game.py
@@ -3,3 +3,25 @@ class Game():
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+
+
+def play_game(player_1_choice, player_2_choice):
+    if player_1_choice == "Rock" and player_2_choice == "Scissors":
+        #player_1 wins
+        
+    if player_1_choice == "Paper" and player_2_choice == "Rock":
+        #player_1 wins
+        
+    if player_1_choice == "Scissors" and player_2_choice == "Paper":
+        #player_1 wins
+
+    if player_2_choice == "Rock" and player_1_choice == "Scissors":
+        #player_2 wins
+        
+    if player_2_choice == "Paper" and player_1_choice == "Rock":
+        #player_2 wins
+        
+    if player_2_choice == "Scissors" and player_1_choice == "Paper":
+        #player_2 wins    
+    
+    else: #return draw
