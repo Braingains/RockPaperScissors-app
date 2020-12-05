8c826203077aca33fbd0fec96bfbe02bb38a8716
Ewan Brain <ewan.brain@gmail.com>
2020-12-05 14:50:27 +0000

    added basic rules page

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,12 @@
 from flask import render_template, request, redirect
 from app import app
+from models.game import *
+from models.player import *
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html', title='Rules')
